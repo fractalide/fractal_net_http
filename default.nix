c94d812f5acb0f9317d62f51cd7363c3f4deb2f0
Stewart Mackenzie <setori88@gmail.com>
2016-10-18 22:06:47 +0800

    Problem: specializations don't point to head
    Solution: point them to head

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ fractalide ? import (fetchTarball https://github.com/fractalide/fractalide/archive/4c2ee3a8acdd6227fc34eded938c3ff205c055d0.tar.gz) {}
+{ fractalide ? import (fetchTarball https://github.com/fractalide/fractalide/archive/master.tar.gz) {}
   , pkgs ? fractalide.pkgs
   , support ? fractalide.support
   , contracts ? fractalide.contracts
@@ -7,5 +7,4 @@ let
   all_contracts = contracts // import ./contracts {inherit pkgs support all_contracts ;};
   all_components = components // import ./components {inherit pkgs support all_components all_contracts;};
 in
-# the top level subnet you want to expose to the fractalide repo
 all_components.vendor_test_nand
