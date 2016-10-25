c8fdc3869e50cfe413c73b57425cc3de84cb87dc
Stewart Mackenzie <setori88@gmail.com>
2016-10-26 00:09:32 +0800

    Problem: users need to quickly and easily add components
    Solution: add a utls directory containing new_component.py

--- components/default.nix
+++ components/default.nix
@@ -1,9 +1,9 @@
 { pkgs, support, allContracts, allComponents, ... }:
 let
 callPackage = pkgs.lib.callPackageWith (pkgs // support // allContracts // allComponents);
-self = rec {
+self = rec { # use one line only to insert a component (utils/new_component.py sorts this list)
   example_satellite_repo_nand = callPackage ./example/satellite/repo/nand {};
   example_satellite_repo_test = callPackage ./example/satellite/repo/test {};
-};
+}; # use one line only to insert a component (utils/new_component.py sorts this list)
 in
 self
