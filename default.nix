66ad3bf74b04627edc71227b3e5b944561854367
Stewart Mackenzie <setori88@gmail.com>
2016-11-04 18:27:01 +0800

    Problem: default.nix needs to be as copy-pasteable as possible
    Solution: now only one line needs to be changed.

--- default.nix
+++ default.nix
@@ -2,14 +2,19 @@
   , pkgs ? fractalide.pkgs
   , support ? fractalide.support
   , contracts ? fractalide.contracts
-  , components ? fractalide.components}:
+  , components ? fractalide.components
+  , fractalide_user ? null
+  , config_file ? null}:
 let
-  exeSubnet = allComponents.net_http;
-  publicNamespace = {components = fracComponents; contracts = fracContracts;};
+  exeSubnet = fracComponents.test;
+  publicNamespace = { components = fracComponents; contracts = fracContracts; };
   fracContracts = import ./contracts {inherit pkgs support allContracts;};
   fracComponents = import ./components {inherit pkgs support allContracts allComponents;};
   allContracts = contracts // fracContracts;
-  allComponents = components // fracComponents;
+  allComponents = components // fracComponents // { encrypt = encrypt; };
+  encrypt = support.encryptComponent {
+      fractalide_user = fractalide_user;
+      keybase_config_file = config_file; };
   result = if fractalide == null
     then publicNamespace
     else import (<fractalide> + "/support/vm/") {inherit pkgs support;
